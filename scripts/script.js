3f10771d5363138a1fe83c63937deeeba5a24c4b
denise-scholten <denise.scholten@hva.nl>
2025-12-02 18:39:44 +0100

    Add files via upload
    nieuwe versie van de website (werkend carousel en menu)

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,39 @@
-// JavaScript Document
-console.log("hi");
-// hamburger menu hulp van ella met de code// 
+// // JavaScript Document
+// console.log("hi");
+// // hamburger menu hulp van ella met de code// 
 
-const menu = document.getElementById("menu");
-const nav = document.querySelector("nav");
+// const menu = document.getElementById("menu");
+// const nav = document.querySelector("nav");
 
-const openBtn = document.querySelector("header button");
-const closeBtn = document.querySelector("nav button");
+// const openBtn = document.querySelector("header button");
+// const closeBtn = document.querySelector("nav button");
 
-// Menu openen
+// // Menu openen
 
-openBtn.addEventListener("click", () => {
-   menu.hidden = false;
-   nav.classList.add("open")
-   })
+// openBtn.addEventListener("click", () => {
+//    menu.hidden = false;
+//    nav.classList.add("open")
+//    })
 
-// Menu sluiten
+// // Menu sluiten
 
-closeBtn.addEventListener("click", () => {
-   nav.classList.remove("open")
-     // Wacht tot de animatie klaar is voordat hidden terug wordt gezet
-        setTimeout(() => {
-            menu.hidden = true;
-        }, 300)
-    })
+// closeBtn.addEventListener("click", () => {
+//    nav.classList.remove("open")
+//      // Wacht tot de animatie klaar is voordat hidden terug wordt gezet
+//         setTimeout(() => {
+//             menu.hidden = true;
+//         }, 300)
+//     })
 
  
+const menuButton = document.querySelector("header div > button");
+const closeButton = document.querySelector("nav > button");
+const header = document.querySelector("header");
+
+menuButton.addEventListener("click", () => {
+    header.classList.toggle("open");
+});
+
+closeButton.addEventListener("click", () => {
+    header.classList.remove("open");
+});
